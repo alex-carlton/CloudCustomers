3484455aa852e62477e3f5e9c91e2845311d5d07
Alex Carlton <ahcarlton@gmail.com>
2022-06-20 16:45:35 -0400

    Add XML comments on Service Models

--- CloudCustomers.Service/Models/Address.cs
+++ CloudCustomers.Service/Models/Address.cs
@@ -1,9 +1,23 @@
 ﻿namespace CloudCustomers.Services.Models
 {
+    /// <summary>
+    /// To mark directions for delivery on
+    /// </summary>
     public class Address
     {
-        public string Street { get; set; }
-        public string City { get; set; }
-        public string ZipCode { get; set; }
+        /// <summary>
+        /// A thoroughfare especially in a city, town, or village that is wider than an alley or lane and that usually includes sidewalks
+        /// </summary>
+        public string? Street { get; set; }
+
+        /// <summary>
+        /// An inhabited place of greater size, population, or importance than a town or village
+        /// </summary>
+        public string? City { get; set; }
+
+        /// <summary>
+        /// A number that identifies a particular postal delivery area in the U.S.
+        /// </summary>
+        public string? ZipCode { get; set; }
     }
 }
--- CloudCustomers.Service/Models/User.cs
+++ CloudCustomers.Service/Models/User.cs
@@ -1,10 +1,28 @@
 ﻿namespace CloudCustomers.Services.Models
 {
+    /// <summary>
+    /// One that uses
+    /// </summary>
     public class User
     {
+        /// <summary>
+        /// Identifier
+        /// </summary>
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public Address Address { get; set; }
+
+        /// <summary>
+        /// A word or phrase that constitutes the distinctive designation of a person or thing
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// A means or system for transmitting messages electronically (as between computers on a network)
+        /// </summary>
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// To mark directions for delivery on
+        /// </summary>
+        public Address? Address { get; set; }
     }
 }
