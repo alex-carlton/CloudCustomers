4aed9927a9c339e77941a46c0c4f3b44a73b3ca3
Alex Carlton <ahcarlton@gmail.com>
2022-06-20 17:22:31 -0400

    Refactored GetAllUsers Return and Added XML Comment

--- CloudCustomers.Service/UsersService.cs
+++ CloudCustomers.Service/UsersService.cs
@@ -1,10 +1,15 @@
 ﻿using CloudCustomers.Services.Models;
+using System;
 using System.Net.Http.Json;
 
 namespace CloudCustomers.Services
 {
     public interface IUsersService
     {
+        /// <summary>
+        /// Get All Users
+        /// </summary>
+        /// <returns>List of User Data</returns>
         public Task<List<User>> GetAllUsers();
     }
 
@@ -24,11 +29,41 @@ namespace CloudCustomers.Services
 
             if (usersResponse.StatusCode == System.Net.HttpStatusCode.NotFound)
             {
-                return new List<User>();
+                return new List<User>()
+                { 
+                    new User()
+                    {
+                        Id = 0,
+                        Name = "Jack",
+                        Email = "jack@text.com",
+                        Address = new Address()
+                        {
+                            Street = "123 Main Street",
+                            City = "Greenville",
+                            ZipCode = "55555"
+                        }
+                    },
+                    new User()
+                    {
+                        Id = 1,
+                        Name = "Jill",
+                        Email = "jill@text.com",
+                        Address = new Address()
+                        {
+                            Street = "123 Main Street",
+                            City = "Greenville",
+                            ZipCode = "55555"
+                        }
+                    },
+                };
             }
 
-            var responseContent = usersResponse.Content;
-            return (await responseContent.ReadFromJsonAsync<List<User>>()).ToList();
+            var users = await usersResponse.Content.ReadFromJsonAsync<List<User>>();
+
+            if (users == null)
+                throw new NullReferenceException(nameof(users));
+
+            return users.ToList();
         }
     }
 }
